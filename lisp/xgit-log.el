e4604efe3ecfc3033308d69bac1eb19c471b5c65
Takuzo O'hara <takuzo.ohara@gmail.com>
2007-06-03 21:52:15 +0900

    Adds better support for xgit-log mode.
      -. Adds customizable max count to xgit-log, as git repositories like
         Linux kernel have huge logs and listing all logs is heavy on slow
         machines.
    
      -. Adds xgit-show-revision-at-point, bound to '='.
    
      -. Adds xgit-describe-revision-at-point, bound to 'd'.
    
      -. Adds utilities in xgit-log mode:
         -. Filter logs to ones with log message that matches the
            specified pattern. (git-log-grep)
         -. Filter logs to ones related to specified
            filename. (git-log-file)
         -. Filter logs to ones whose diffs matche given
            pattern. (git-log-diff-grep)
         -. Find log for the specified commit. (git-log-revision)
    
      -. --pretty= option can be passed by customizable variable to
         git-log(1).  You can choose from 'oneline', 'short', 'medium',
         'full', 'fuller', 'email' or 'raw', and output are parsable by
         xgit-log.
    
      -. Added more patterns for font-lock.

--- lisp/xgit-log.el
+++ lisp/xgit-log.el
@@ -32,8 +32,14 @@
 (defvar xgit-log-mode-map
   (let ((map (make-sparse-keymap)))
     (define-key map dvc-keyvec-help 'describe-mode)
+    (define-key map [?=] 'xgit-show-revision-at-point)
     (define-key map [?g] 'xgit-log)
     (define-key map [?s] 'xgit-status)
+    (define-key map [?G] 'xgit-log-grep)
+    (define-key map [?F] 'xgit-log-file)
+    (define-key map [?S] 'xgit-log-diff-grep)
+    (define-key map [?I] 'xgit-log-revision)
+    (define-key map [?d] 'xgit-describe-revision-at-point)
     (define-key map dvc-keyvec-next 'xgit-log-next)
     (define-key map dvc-keyvec-previous 'xgit-log-previous)
     (define-key map dvc-keyvec-quit 'dvc-buffer-quit)
@@ -49,11 +55,19 @@
 
 
 (defvar xgit-log-font-lock-keywords
-   '(("^committer" . font-lock-function-name-face)
+  '(("^[Cc]ommit[ter]*[Date]*[:]?" . font-lock-function-name-face)
      ("^commit" . font-lock-function-name-face)
      ("^tree" . font-lock-function-name-face)
      ("^parent" . font-lock-function-name-face)
-     ("^author" . font-lock-function-name-face))
+     ("^[Aa]uthor[Date]*[:]?" . font-lock-function-name-face)
+     ("^Date:" . font-lock-function-name-face)
+     ("^From[:]?" . font-lock-function-name-face)
+     ("^Subject" . font-lock-function-name-face)
+     ("^Merge:" . font-lock-function-name-face)
+     ("^No tag:" . font-lock-function-name-face)
+     ("^Tag: .*$" . font-lock-warning-face)
+     ("^    .*$" . font-lock-type-face)
+     )
   "Keywords in `xgit-log-mode' mode.")
 
 
@@ -67,26 +81,76 @@ Commands:
        (list 'xgit-log-font-lock-keywords t nil nil))
   (toggle-read-only 1))
 
-(defconst xgit-log-start-regexp "^commit +\\([0-9a-f]+\\)")
+;; Regexp to match output of git log.
+;; Matches to:
+;; --pretty=oneline
+;;   a679aff48d8abf688c53720cb626d2ab00ebd53d <First line of commit>
+;; or
+;;   commit a679aff48d8abf688c53720cb626d2ab00ebd53d
+(defconst xgit-log-start-regexp
+  "^commit +\\([[:xdigit:]]\\{40\\}\\)\\|^\\([[:xdigit:]]\\{40\\}\\)\\|From \\([[:xdigit:]]\\{40\\}\\)")
+
 (defun xgit-log-next (n)
   "Move to the next changeset header of the next diff hunk"
   (interactive "p")
-  (end-of-line)
-  (re-search-forward xgit-log-start-regexp nil t n)
-  (beginning-of-line))
+  (let ((retval))
+    (end-of-line)
+    (setq retval (re-search-forward xgit-log-start-regexp nil t n))
+    (beginning-of-line)
+    retval))
 
 (defun xgit-log-previous (n)
   "Move to the previous changeset header of the previous diff hunk"
   (interactive "p")
-  (end-of-line)
-  (re-search-backward xgit-log-start-regexp)
-  (re-search-backward xgit-log-start-regexp nil t n))
+  (let ((retval))
+    (end-of-line)
+    (re-search-backward xgit-log-start-regexp)
+    (setq retval (re-search-backward xgit-log-start-regexp nil t n))
+    retval))
 
 (defun xgit-log-revision-at-point ()
+  (interactive)
   (save-excursion
     (end-of-line)
     (re-search-backward xgit-log-start-regexp)
-    (match-string-no-properties 1)))
+    (cond ((string= "oneline" git-log-pretty)
+           (match-string-no-properties 2))
+          ((string= "email" git-log-pretty)
+           (match-string-no-properties 3))
+          (t
+           (match-string-no-properties 1)))))
+
+(defun xgit-show-revision-at-point (n)
+  "Shows diff for a given revision"
+  (interactive "p")
+  (let ((tmp git-show-filter-filename-func))
+    (if (> n 1) (setq tmp nil))
+    (let ((git-show-filter-filename-func tmp))
+      (xgit-show default-directory (xgit-log-revision-at-point))))
+  (xgit-describe-revision-at-point))
+
+(defun xgit-describe-revision-at-point ()
+  "Shows most recent tag for a given revision"
+  (interactive)
+  (end-of-line)
+  (re-search-backward xgit-log-start-regexp)
+  (let ((info (xgit-describe default-directory (xgit-log-revision-at-point))))
+    (when info
+      (toggle-read-only 0)
+      (if (stringp info)
+	  (insert (format "Tag: %s\n" info))
+	(insert (format "No tag: %s\n" (nth 2 info))))
+      (toggle-read-only 1)
+      info)))
+
+(defun xgit-describe-all-revision ()
+  "Shows most recent tag for all revisions in a buffer."
+  (interactive)
+  (save-excursion
+    (goto-char (point-min))
+    (while (xgit-log-next 1)
+      (xgit-describe-revision-at-point)
+      )))
 
 (provide 'xgit-log)
 ;;; xgit-log.el ends here
