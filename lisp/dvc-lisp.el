9d9e018f9aebead7419e52998c3bf56fcb6279c5
Michael W. Olson <mwolson@gnu.org>
2007-08-26 21:27:23 -0400

    Make dvc-capturing-lambda reuse already-captured symbols
    * lisp/dvc-lisp.el (dvc-capturing-lambda-helper): Reuse already-captured
      symbols.  Push a cons cell rather than a list, because otherwise rassoc
      will not work.  Throw an error if trying to capture a non-symbol.
      (dvc-capturing-lambda): Get the cdr of the captured value entry, rather
      than the cadr, since we are now using a cons cell.

--- lisp/dvc-lisp.el
+++ lisp/dvc-lisp.el
@@ -57,9 +57,14 @@
   (defun dvc-capturing-lambda-helper (l)
     (cond ((atom l) l)
           ((eq (car l) 'capture)
-           (let ((g (dvc-gensym)))
-             (push (list g (cadr l)) captured-values)
-             g))
+           (let ((sym (cadr l)))
+             (unless (symbolp sym)
+               (error "Expected a symbol in capture statement: %S" sym))
+             (let ((g (car (rassq sym captured-values))))
+               (unless g
+                 (setq g (dvc-gensym))
+                 (push (cons g sym) captured-values))
+               g)))
           (t (mapcar 'dvc-capturing-lambda-helper l))))
 
   (defmacro dvc-capturing-lambda (args &rest body)
@@ -83,7 +88,7 @@ environment.
              (list 'let (list
                          ,@(mapcar (lambda (var)
                                      `(list ',(car var)
-                                            (list 'quote ,(cadr var))))
+                                            (list 'quote ,(cdr var))))
                                    captured-values))
                    (list 'funcall (lambda () . ,body))))))
   (put 'dvc-capturing-lambda 'lisp-indent-function 1)
