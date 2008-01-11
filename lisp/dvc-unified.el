5ba57a549d19d6de4ccc79aa7afbbff64b2c58ea
Stephen Leake <stephen_leake@stephe-leake.org>
2008-01-11 03:55:27 -0500

    Improve documentation of relation of dvc-log-edit to dvc-current-file-list.
    * lisp/dvc-core.el (dvc-current-file-list): Doc that dvc-log-edit must
      match this function.
      (dvc-dvc-files-to-commit): Add FIXME.
    
    * lisp/dvc-unified.el (dvc-log-edit): Doc that `default-directory' must
      be the tree root. Improve comment about dvc-diff or status buffers.
    
    * lisp/xgit.el (xgit-parse-status): Fix free variable `indexed'.

--- lisp/dvc-unified.el
+++ lisp/dvc-unified.el
@@ -269,7 +269,8 @@ the current active back-end."
   "Edit the log before commiting. Optional OTHER_FRAME (default
 user prefix) puts log edit buffer in a separate frame. Optional
 NO-INIT if non-nil suppresses initialization of buffer if one is
-reused."
+reused.
+`default-directory' must be the tree root."
   (interactive "P")
   (setq other-frame (Xor other-frame dvc-log-edit-other-frame))
   ;; Reuse an existing log-edit buffer if possible.
@@ -286,9 +287,10 @@ reused."
   (let ((log-edit-buffers (dvc-get-matching-buffers dvc-buffer-current-active-dvc 'log-edit default-directory)))
     (case (length log-edit-buffers)
       (0 ;; Need to create a new log-edit buffer. In the log-edit
-         ;; buffer, dvc-partner-buffer must be set to a dvc-diff or
-         ;; dvc-status buffer. So if we are not currently in one of
-         ;; those, find one.
+         ;; buffer, dvc-partner-buffer must be set to a buffer with a
+         ;; mode that dvc-current-file-list supports. That is
+         ;; currently dvc-diff-mode or dired-mode; we don't have a way
+         ;; to find dired-mode buffers, so we ignore those.
        (let ((diff-status-buffers
               (append (dvc-get-matching-buffers dvc-buffer-current-active-dvc 'diff default-directory)
                       (dvc-get-matching-buffers dvc-buffer-current-active-dvc 'status default-directory))))
