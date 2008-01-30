0f348baaf29061b1a50781bc37389ebc152eb8aa
Stephen Leake <stephen_leake@stephe-leake.org>
2008-01-30 04:41:04 -0500

    lisp/dvc-emacs.el: Alphabetize emacs 22 features

--- lisp/dvc-emacs.el
+++ lisp/dvc-emacs.el
@@ -96,6 +96,7 @@
 (defconst dvc-mouse-face-prop 'mouse-face)
 
 ;; Provide features from Emacs 22 for Emacs 21
+;; alphabetical by symbol name
 
 (if (fboundp 'ewoc-delete)
     (defalias 'dvc-ewoc-delete 'ewoc-delete)
@@ -120,14 +121,6 @@
               (ewoc--node-left  node) nil
               (ewoc--node-right node) nil)))))
 
-(if (fboundp 'redisplay)
-    (defalias 'dvc-redisplay 'redisplay)
-  (defun dvc-redisplay (&optional force)
-    (if force
-        (let ((redisplay-dont-pause t))
-          (sit-for 0))
-      (sit-for 0))))
-
 (if (fboundp 'line-number-at-pos)
     (defalias 'dvc-line-number-at-pos 'line-number-at-pos)
   (defun dvc-line-number-at-pos (&optional pos)
@@ -140,6 +133,15 @@ If POS is nil, use current buffer location."
         (goto-char opoint)
         (forward-line 0)
         (1+ (count-lines start (point)))))))
+
+(if (fboundp 'redisplay)
+    (defalias 'dvc-redisplay 'redisplay)
+  (defun dvc-redisplay (&optional force)
+    (if force
+        (let ((redisplay-dont-pause t))
+          (sit-for 0))
+      (sit-for 0))))
+
 
 ;; FIXME: move to dvc-utils?
 (defun dvc-emacs-make-temp-dir (prefix)
