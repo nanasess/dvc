11244b12eb23ca8a385c9455bec6a6bddc313076
Stefan Reichoer <stefan@xsteve.at>
2008-02-27 22:33:54 +0100

    Implemented a simple xgit-pull command

--- lisp/xgit.el
+++ lisp/xgit.el
@@ -448,9 +448,17 @@ many generations back we want to go from the given commit ID.")
     (with-current-buffer buffer (goto-char (point-min)))
     buffer))
 
+;;;###autoload
 (defun xgit-fetch (&optional repository)
   "Call git fetch."
+  (interactive "sGit fetch from: ")
   (dvc-run-dvc-async 'xgit (list "fetch" repository)))
+
+;;;###autoload
+(defun xgit-pull (&optional repository)
+  "Call git pull."
+  (interactive "sGit pull from: ")
+  (dvc-run-dvc-async 'xgit (list "pull" repository)))
 
 (defun xgit-split-out-added-files (files)
   "Remove any files that have been newly added to git from FILES.
